9eb36daebbf831991c37bc27ab934dbb73bdd937
laoshay <105021050@live.asia.edu.tw>
2017-02-24 14:58:41 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,13 @@
+/*
+* Topic:自我介紹
+* Date: 2017/02/24
+* Author: 105021050 薛嘉華
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("資工1A資電組    學號:105021050  姓名:薛嘉華");
+	}
+
+}
